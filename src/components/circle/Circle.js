b083a42a2506dfe3554d6667ad6c352d950b20f5
CarlottaFriedmann <friedmanncarlotta@gmail.com>
2020-09-02 21:43:36 +0200

    use state switching colors

--- src/components/circle/Circle.js
+++ src/components/circle/Circle.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './circle.css'
 
 
 function Circle() {
+
+  const [color,setColor]= useState('#8a2be2');
+
+  const changeColor = (e)=> {
+    e.preventDefault()
+    setColor('#2be29c') 
+  }
+
     return (
-      <div className="circle">
-     
+      <div className="circle" style ={{backgroundColor: color}} onClick={changeColor}>
       </div>
     );
   }
