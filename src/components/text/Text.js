4da827bd5db709f0c8339f2a06dcf1ed898a00c2
CarlottaFriedmann <friedmanncarlotta@gmail.com>
2020-09-02 19:52:44 +0200

    test message

--- src/components/text/Text.js
+++ src/components/text/Text.js
@@ -4,7 +4,7 @@ function Text() {
     return (
       <div className="Text">
 
-        <h1 style={{textAlign:"center", marginTop:"50px"}}>Hello</h1>
+        <h1 style={{textAlign:"center", marginTop:"50px"}}>Click and Color!!</h1>
         
       </div>
     );
